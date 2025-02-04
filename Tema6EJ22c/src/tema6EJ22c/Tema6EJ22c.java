236a77f0c6690820729344d5aa281e3e4a3abed7
alvaro <alvaroch975@gmail.com>
2025-02-04 10:37:20 +0100

    skibidi Signed-off-by: alvaro <alvaroch975@gmail.com>

---
+++ Tema6EJ22c/src/tema6EJ22c/Tema6EJ22c.java
@@ -0,0 +1,18 @@
+package tema6EJ22c;
+
+import java.util.HashMap;
+
+public class Tema6EJ22c {
+
+	class Palabra {
+		String termino;
+		String definicion;
+		}
+	
+	public static void main(String[] args) {
+		HashMap<String,String> diccionario=new HashMap<>();
+		// TODO Auto-generated method stub
+
+	}
+
+}
